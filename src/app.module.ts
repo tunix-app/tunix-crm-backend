bcd8f9013e7548b88cfba9d84aa851066c12fb04
Jarvis Celestial <jcelestial198@gmail.com>
2025-08-19 00:32:28 -0500

    add auth features using supabase JWT

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from './config/config.module';
+import { ConfigModule } from '@nestjs/config';
 import { KnexModule } from './infra/database/knex.module';
 import { HealthModule } from './health/health.module';
+import { AuthModule } from './auth/auth.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule, KnexModule, HealthModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), KnexModule, HealthModule, AuthModule],
+  controllers: [AppController],
+  providers: [AppService]
 })
 export class AppModule {}
