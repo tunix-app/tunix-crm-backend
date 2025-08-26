ee8b3b4f826f5c24bced859481a6458aee9c38e2
Jarvis Celestial <jcelestial198@gmail.com>
2025-08-25 22:31:16 -0500

    linter warning

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -38,6 +38,7 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-return': 'warn', // Warn on unsafe assignments
       '@typescript-eslint/no-unsafe-call': 'warn', // Warn on unsafe assignments
       '@typescript-eslint/no-unsafe-member-access': 'warn',
+      '@typescript-eslint/require-await': 'warn',
     },
   },
 );
